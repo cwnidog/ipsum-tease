eda782e150b5bd028c623a137481d8953b0390a4
Forrest Parker <forresta.parker@gmail.com>
2014-08-28 16:38:15 -0700

    working site

--- js/login.js
+++ js/login.js
@@ -1,12 +1,6 @@
 var users = ['username' , 'forrest', 'john', 'sam'];
 var loggedinuser = "";
 
-function signupForm() {
-  $("div.login form").empty();
-  $("div.login").append("<form><div><label>Username:</label><input type='text' id='named' name='username'/></div><div><button id='signedup'>Signed up</button></div></form>");
-//borrowed form guts     action='/signup' method='post'
-}
-
 function loginForm() {
   $("div.login form").empty();
   $("div.login").append("<form><div><label>Username:</label><input type='text' id='named' name='username'/></div><div><button id='loggedin'>Logged In</button></div></form>");
@@ -14,15 +8,27 @@ function loginForm() {
 }
 
 function loggedIn() {
-  var checkname=$("div.login #named").val();
-  for(var i = 0; i < users.length; i++) {
+  var checkname = $("div.login #named").val();
+  if(localStorage.getItem('loggedinuser') === null) {
+    $("div.login").append("<form><button id='login-pop'>Login</button></form>");
+    $("button#login-pop").on("click", function(){
+      loginForm();
+      stepTwoLogin();
+    });
+  }
+  else{
 
+  for(var i = 0; i < users.length; i++) {
     /*check local storage for a user*/
     var tempobj = JSON.parse(localStorage.getItem('loggedinuser'));
     if(tempobj.name == users[i]) {
-      loggedinuser = tempobj;
+      loggedinuser = JSON.parse(localStorage.getItem('loggedinuser'));
       $("div.login form").empty();
-      $("div.login").append("<div><label id='userlogedin'>Welcome Back "+loggedinuser.name+"</label></div></form>");
+      $("div.login").append("<div><label id='userlogedin'>Welcome Back "+loggedinuser.name+"</label></div><button id='signout'>Signout</button></form>");
+      $("#signout").on("click", function() {
+        localStorage.clear();
+        loggedIn();
+      });
       break;
     }
 
@@ -33,17 +39,14 @@ function loggedIn() {
       loggedinuser = new User(loggedinuser);
       $("div.login form").empty();
       localStorage.setItem('loggedinuser', JSON.stringify(loggedinuser));
-      $("div.login").append("<div><label id='userlogedin'>Welcome Back "+loggedinuser.name+"</label></div></form>");
+      $("div.login").append("<div><label id='userlogedin'>Welcome Back "+loggedinuser.name+"</label></div><button id='signout'>Signout</button></form>");
+      $("#signout").on("click", function() {
+        localStorage.clear();
+        loggedIn();
+      });
       break;
     }
-
-    else {
-    $("div.login").append("<form><button id='login-pop'>Login</button></form>");
-    $("button#login-pop").on("click", function(){
-      loginForm();
-      stepTwoLogin();
-    });
-    }
+  }
   }
 }
 
@@ -63,10 +66,11 @@ $(document).ready(function(){
 
 function stepTwoLogin() {
   $("button#signedup").on("click", function(){
-    newUser();
+    loggedIn();
   });
 
   $("button#loggedin").on("click", function(){
+    localStorage.setItem('loggedinuser', JSON.stringify(loggedinuser));
     loggedIn();
   });
 
