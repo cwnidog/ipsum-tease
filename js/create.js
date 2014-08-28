f201aac0dca3351f4f3d0161ddf5aaa0b4667bce
John Leonard <cwnidog1@gmail.com>
2014-08-28 13:10:34 -0700

    removing extra .js files, fixing HTML shopping cart error

--- js/create.js
+++ js/create.js
@@ -208,10 +208,10 @@ $(document).ready(function(){
     $("#shirtText").text("Lo-fi Etsy wolf, Tumblr food truck single-origin coffee irony Helvetica sartorial cornhole. Biodiesel fanny pack stumptown mlkshk, forage chia art party farm-to-table viral ethical vinyl.");
   });
 
-  /* When the create button is pushed, we'll instantiate a new TShirt and
+  /* When the cart button is pushed, we'll instantiate a new TShirt and
      populate its attributes with values the user has provided. */
 
-  $("#btnCreate").click(function() {
+  $("#cart").click(function() {
 
     // get a new instance of TShirt
     var tShirt = new TShirt();
@@ -222,7 +222,7 @@ $(document).ready(function(){
     tShirt.setStyle($("#createStyle").val);
     thsirt.setColor($("#createColor").val);
     tShirt.setText($("#shirtText").val);
-  }); // when the "Create" button is clicked
+  }); // when the "Cart" button is clicked
 
 
 }); // when document ready
